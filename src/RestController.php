737a5332d3ee9dc6ac3516a96b325d3e065a78d2
Kelvin Wong <kelvinwonggg@gmail.com>
2020-07-30 16:41:58 +0800

    basic rest controller

--- src/RestController.php
+++ src/RestController.php
@@ -5,10 +5,50 @@
 // namespace HelloComposer;
 namespace kelvinwongg\CIRestController;
 
-class RestController
-{
-    public function say($toSay = "Nothing given")
-    {
-        return $toSay;
+class RestController extends CI_Controller {
+
+  private $endpoint;
+  private $verb;
+  private $params;
+  
+  public function __construct() {
+    parent::__construct();
+    $this->endpoint = $this->router->fetch_class();
+    $this->verb = $this->input->method();
+    $this->params = $this->uri->uri_to_assoc(1);
+    $this->load->library( 'oauth2' );
+    $this->load->model( $this->endpoint . '_model', 'model' );
+  }
+
+  public function _remap($method, $params = []) {
+
+    // If $method exist, call it,
+    // except 'index' default method
+    if ( $method !== 'index' && method_exists($this, $method) ) {
+      call_user_func([$this, $method], $params);
     }
+
+    // Check if the function exists for this verb
+    if ( !method_exists($this, $this->verb) ) {
+      response_json([
+        'status' => 400,
+        'message' => 'The requested HTTP Method is not supported in this endpoint.'
+      ]);
+      return;
+    }
+    
+    // Call the verb with parameters
+    call_user_func([$this, $this->verb], $this->params);
+
+  }
+
+  public function index() {
+    echo $this->endpoint . '/index()';
+  }
+
+  public function get($params) {
+    $this->oauth2->handleResourceRequest('basic staff');
+    $ret = $this->model->get($this->endpoint, $params[$this->endpoint]);
+    response_json($ret);
+  }
 }
